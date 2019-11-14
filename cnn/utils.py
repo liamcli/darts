689f2d65095c13457566721e28e46f7a81ca7579
liam <me@liamcli.com>
2019-11-14 08:25:02 +0000

    updates for pytorch 0.4 compatibility

--- cnn/utils.py
+++ cnn/utils.py
@@ -5,24 +5,6 @@ import shutil
 import torchvision.transforms as transforms
 from torch.autograd import Variable
 
-import boto3
-
-def upload_to_s3(source, bucket, key):
-    s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(source, bucket, key)
-
-def download_from_s3(key, bucket, target):
-    # Make sure directory exists before downloading to it.
-    target_dir = os.path.dirname(target)
-    if len(target_dir) and not os.path.exists(target_dir):
-        os.makedirs(target_dir)
-
-    s3 = boto3.resource('s3')
-    try:
-      s3.meta.client.download_file(bucket, key, target)
-    except Exception as e:
-      print(e)
-
 class AvgrageMeter(object):
 
   def __init__(self):
--- rnn/utils.py
+++ rnn/utils.py
@@ -3,11 +3,12 @@ import torch.nn as nn
 import os, shutil
 import numpy as np
 from torch.autograd import Variable
+from torch import Tensor
 
 
 def repackage_hidden(h):
-    if type(h) == Variable:
-        return Variable(h.data)
+    if type(h) == Tensor:
+            return torch.tensor(h.data, requires_grad=True) 
     else:
         return tuple(repackage_hidden(v) for v in h)
 
@@ -16,7 +17,6 @@ def batchify(data, bsz, args):
     nbatch = data.size(0) // bsz
     data = data.narrow(0, 0, nbatch * bsz)
     data = data.view(bsz, -1).t().contiguous()
-    print(data.size())
     if args.cuda:
         data = data.cuda()
     return data
@@ -24,7 +24,7 @@ def batchify(data, bsz, args):
 
 def get_batch(source, i, args, seq_len=None, evaluation=False):
     seq_len = min(seq_len if seq_len else args.bptt, len(source) - 1 - i)
-    data = Variable(source[i:i+seq_len], volatile=evaluation)
+    data = Variable(source[i:i+seq_len])
     target = Variable(source[i+1:i+1+seq_len])
     return data, target
 
@@ -64,7 +64,7 @@ def embedded_dropout(embed, words, dropout=0.1, scale=None):
     padding_idx = embed.padding_idx
     if padding_idx is None:
         padding_idx = -1
-    X = embed._backend.Embedding.apply(words, masked_embed_weight,
+    X = torch.nn.functional.embedding(words, masked_embed_weight,
         padding_idx, embed.max_norm, embed.norm_type,
         embed.scale_grad_by_freq, embed.sparse
     )
